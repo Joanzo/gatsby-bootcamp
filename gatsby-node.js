1ad4e3cd4b37a2444a4700de26e56076ce46e22c
joanzo <jo@joanzo.com>
2020-03-06 21:39:31 +0800

    remove mark down

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,17 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
+//   if (node.internal.type === "MarkdownRemark") {
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: slug,
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
